2f195c8fd347c42065ca07d3712f5ba8b972735a
maironcorrales <maironcorrales@gmail.com>
2017-05-07 21:25:57 -0600

    Login y revisada

---
+++ BSFamilyProject/BSFamilyProject/Forms/Conta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BSFamilyProject.Forms
+{
+    public partial class Conta : Form
+    {
+        public Conta()
+        {
+            InitializeComponent();
+        }
+
+        private void Conta_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
